2f52951d065253f37a345b5516deafb5445c6022
joao.grilo <joao.grilo@meta.com.br>
2022-10-19 16:52:43 -0300

    Manipulação básica de um objeto

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+
+        primeiraConta.saldo = 200;
+        System.out.printf("Saldo: " + primeiraConta.saldo);
+    }
+}
