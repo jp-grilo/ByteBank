5afce7840bccdf7d8df81e79624e4ec1d96d7ed7
joao.grilo <joao.grilo@meta.com.br>
2022-10-19 17:54:35 -0300

    Estudo de referenciação de objetos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         Conta primeiraConta = new Conta();
+        Conta segundaConta = new Conta();
 
         primeiraConta.saldo = 200;
-        System.out.printf("Saldo: " + primeiraConta.saldo);
+        System.out.println("Saldo: " + primeiraConta.saldo);
+
+        Conta contaReferencia = primeiraConta;
+
+        contaReferencia.saldo=300;
+        System.out.println("Saldo dentro da primeira conta: " + primeiraConta.saldo +
+                "\nSaldo dentro da conta referencia: " + contaReferencia.saldo);
+
+        System.out.println("As contas primeiraConta e contaReferencia são a mesma coisa?\nR: " + primeiraConta.equals(contaReferencia));
+        System.out.println("As contas primeiraConta e segundaConta são a mesma coisa?\nR: " + primeiraConta.equals(segundaConta));
+
     }
 }
